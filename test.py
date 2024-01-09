d3d0b6aa90f67e03e5120d445d21dea163de4ec1
vobobos <vebjorn.landmark@outlook.com>
2024-01-09 09:06:03 +0100

    jeg tror jeg fikk til noe

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 
 # Oppgave 1:
 import csv
+import locale
 with open("konsumprisindex.csv", encoding="utf-8-sig") as fil:
     filinnhold = csv.reader(fil, delimiter=";")
     head = next(filinnhold)
@@ -10,7 +11,20 @@ with open("konsumprisindex.csv", encoding="utf-8-sig") as fil:
     data = []
     for i in filinnhold:
         data.append(i)
-    print(data)
+    
+    # data[0][2] = float(data[0][2])
+    # setlocale gjør at den fungerer og kan gjøre komma til dot
+    locale.setlocale(locale.LC_ALL, '')
+    for i in data:
+        for x in range(len(i)):
+            if i[x] == '.':
+                i[x] = ""
+            elif x != 0:
+                i[x] = locale.atof(i[x])
+            else:
+                i[x] = int(i[x])
+    for i in data:
+        print(i)
 
 # Oppgave 2:
 # Oppgave 1: Vebjørn og Mathilde
