a18dbc8354ba0f5dd7594985c16288418d4cc2b3
vobobos <vebjorn.landmark@outlook.com>
2024-01-09 08:48:31 +0100

    se så mange data

--- test.py
+++ test.py
@@ -4,7 +4,9 @@
 import csv
 with open("konsumprisindex.csv", encoding="utf-8-sig") as fil:
     data = csv.reader(fil, delimiter=";")
-print(data)
+    head = next(data)
+    for i in data:
+        print(i)
 
 # Oppgave 2:
 # Oppgave 1: Vebjørn og Mathilde
