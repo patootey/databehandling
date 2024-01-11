fa73ef6a0eab153d2d13f620efd2708c01993d20
GustavEndresen <endresengustav@gmail.com>
2024-01-11 13:14:24 +0100

    compak

--- oppgave_2.py
+++ oppgave_2.py
@@ -1,30 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
- 
+
+def parse_yearly_data(data : list):
+    return [float(x.replace(',', '.')) if x != '.' else np.nan for x in data[2:]]
+
+def graph_yearly_average(data : list):
+    years = [int(x[0]) for x in data[1:]]
+    months = [parse_yearly_data(x) for x in data[1:]]
+    averages = [np.nanmean(x) for x in months]
+    plt.plot(years, averages)
+    plt.show()
+
+def graph_months(data : list):
+    year = parse_yearly_data(data[1])
+    month_names = data[0][2:]
+    print(year)
+    # for p in data:
+    #     if not skipped_first:
+    #         skipped_first = True
+    #         continue
+    #     years.append(int(p[0]))
+    pass
+
+
 data = np.loadtxt("konsumprisindex.csv",  delimiter=";", dtype=str).tolist()
-years = []
-months = []
 
-data.pop(0)
-for p in data:
-    years.append(int(p[0]))
-    month = []
-    for i in range(len(p)- 1):
-        d = p[i + 1].replace(",", ".")
-        if (d != '.'):
-            month.append(float(d))
-        else:
-            month.append(np.nan)
-    months.append(month)
+graph_yearly_average(data)
 
-print(years)
-
-averages = []
-
-for m in months:
-    averages.append(np.nanmean(m))
-
-
-plt.plot(years, averages)
-plt.show()
+graph_months(data)
 
