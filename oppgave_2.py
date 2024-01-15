693dd5bf318f0bb3e3292307d510fcf2cbc65c85
GustavEndresen <endresengustav@gmail.com>
2024-01-15 15:55:06 +0100

    Fertig

--- oppgave_2.py
+++ oppgave_2.py
@@ -1,17 +1,28 @@
+# Importerer pyplot fra matplotlib for å tegne grafer
 import matplotlib.pyplot as plt
+# Importerer numpy for å håndtere NaN-verdier
 import numpy as np
 
+"""
+Oppgave 2
+"""
+
+# Hjelpefunksjon som bruker list comprehension for å bytte ut . med , og erstatte tomme elementer med numpy-NaN-verdi
 def parse_yearly_data(data : list):
     return [float(x.replace(',', '.')) if x != '.' else np.nan for x in data[2:]]
 
-def graph_yearly_average(data : list):
+# Del a)
+# Tegner graf av gjennomsnittsverdier for hvert år
+def graph_yearly_averages(data : list):
     years = [int(x[0]) for x in data[1:]]
     months = [parse_yearly_data(x) for x in data[1:]]
     averages = [np.nanmean(x) for x in months]
     plt.plot(years, averages)
     plt.show()
 
-def graph_months(data : list):
+# Del b)
+# Tegner graf av verdier for hver måned i 2022
+def graph_monthly_averages(data : list):
     year = [x for x in parse_yearly_data(data[1])]
     month_names = data[0][2:]
     for i in reversed(range(len(year))):
@@ -20,9 +31,9 @@ def graph_months(data : list):
             month_names.pop(i)
     plt.bar(month_names, year, 0.5)
     plt.show()
-    pass
 
+# Leser data
 data = np.loadtxt("konsumprisindex.csv",  delimiter=";", dtype=str).tolist()
 
-graph_yearly_average(data)
-graph_months(data)
+graph_yearly_averages(data)
+graph_monthly_averages(data)
